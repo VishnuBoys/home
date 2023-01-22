02378f576ed25a1d18d9a7d69ea673289a0cbc7c
VishnuBoys <vishnuboys@gmail.com>
2023-01-22 15:59:36 -0400

    Added Currciulum Table

--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -2,12 +2,12 @@
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import pdf from "../docs/test.pdf"
+import csec from "../docs/csec.pdf"
+import cape from "../docs/cape.pdf"
 export default function MyDropdown() {
   return (
 <div className="">
-<Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
-</Worker>
+
 <div>
 
 <section>
@@ -34,57 +34,6 @@ export default function MyDropdown() {
 </section>
 
 
-
-{/* <div className="overflow-x-auto">
-  <table className="table table-compact w-full">
-   
-    <thead>
-      <tr className="hover">
-        <th></th>
-        <th className="text-amber-500 text-xl">Form Class</th>
-        <th className="text-amber-500 text-xl">Subjects</th>
-      </tr>
-    </thead>
-    <tbody>
-      
-      <tr className="hover">
-        <th></th>
-        <td className="font-bold">Form 1</td>
-        <td className="text-xl">
-          Maths<br/>
-          English<br/>
-          Science<br/>
-          Social Studies<br/>
-          P.E<br/>
-
-        </td>
-  
-      </tr>
-    
-      <tr className="hover">
-        <th></th>
-        <td className="font-bold">Form 2</td>
-        <td className="text-xl">Maths<br/>
-          English<br/>
-          Science<br/>
-          Social Studies<br/>
-          P.E<br/></td>
-        
-      </tr>
-      
-      <tr className="hover">
-        <th></th>
-        <td className="font-bold">Form 3</td>
-        <td className="text-xl">Maths<br/>
-          English<br/>
-          Science<br/>
-          Social Studies<br/>
-          P.E<br/></td>
-
-      </tr>
-    </tbody>
-  </table>
-</div> */}
 
 <div class="flex justify-center bg-gray-100 py-10 p-5">
   <div class="container mr-5 ml-2 mx-auto bg-white shadow-xl">
@@ -192,7 +141,32 @@ export default function MyDropdown() {
 
     </div>
 </div>
+<section>
+    <div class="container px-6 py-10 mx-auto">
+          <div class="lg:flex lg:items-center">
+              <div class="w-full space-y-12 lg:w-1/2 ">
+                  <div>
+                    <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">Subject <br/> Groupings</h1>
 
+                    <div class="mt-2">
+                        <span class="inline-block w-40 h-1 bg-amber-500 rounded-full"></span>
+                        <span class="inline-block w-3 h-1 ml-1 bg-amber-500 rounded-full"></span>
+                        <span class="inline-block w-1 h-1 ml-1 bg-amber-500 rounded-full"></span>
+                    </div>
+                  </div>
+
+                </div>
+
+            </div>
+
+
+
+    </div>
+</section>
+<Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
+<Viewer fileUrl={csec} />;
+<Viewer fileUrl={cape} />;
+</Worker>
 </div>
 
   )
